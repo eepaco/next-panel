8dd4e8ffe31a642cb20be637638689e2795bf735
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2023-07-15 13:13:54 +0330

    Moving animations from tailwind config to CSS file

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,64 +10,7 @@ module.exports = {
 	],
 	theme: {
 		extend: {
-			keyframes: {
-				enterFromRight: {
-					from: { opacity: 0, transform: "translateX(200px)" },
-					to: { opacity: 1, transform: "translateX(0)" },
-				},
-				enterFromLeft: {
-					from: { opacity: 0, transform: "translateX(-200px)" },
-					to: { opacity: 1, transform: "translateX(0)" },
-				},
-				exitToRight: {
-					from: { opacity: 1, transform: "translateX(0)" },
-					to: { opacity: 0, transform: "translateX(200px)" },
-				},
-				exitToLeft: {
-					from: { opacity: 1, transform: "translateX(0)" },
-					to: { opacity: 0, transform: "translateX(-200px)" },
-				},
-				scaleIn: {
-					from: { opacity: 0, transform: "rotateX(-10deg) scale(0.9)" },
-					to: { opacity: 1, transform: "rotateX(0deg) scale(1)" },
-				},
-				scaleOut: {
-					from: { opacity: 1, transform: "rotateX(0deg) scale(1)" },
-					to: { opacity: 0, transform: "rotateX(-10deg) scale(0.95)" },
-				},
-				fadeIn: {
-					from: { opacity: 0 },
-					to: { opacity: 1 },
-				},
-				fadeOut: {
-					from: { opacity: 1 },
-					to: { opacity: 0 },
-				},
-				slideDown: {
-					from: { height: 0 },
-					to: { height: "var(--radix-accordion-content-height)" },
-				},
-				slideUp: {
-					from: { height: "var(--radix-accordion-content-height)" },
-					to: { height: 0 },
-				},
-				slideDownAndFade: {
-					from: { opacity: 0, transform: "translateY(-2px)" },
-					to: { opacity: 1, transform: "translateY(0)" },
-				},
-				slideLeftAndFade: {
-					from: { opacity: 0, transform: "translateX(2px)" },
-					to: { opacity: 1, transform: "translateX(0)" },
-				},
-				slideUpAndFade: {
-					from: { opacity: 0, transform: "translateY(2px)" },
-					to: { opacity: 1, transform: "translateY(0)" },
-				},
-				slideRightAndFade: {
-					from: { opacity: 0, transform: "translateX(-2px)" },
-					to: { opacity: 1, transform: "translateX(0)" },
-				},
-			},
+			keyframes: {},
 			animation: {
 				scaleIn: "scaleIn 200ms ease",
 				scaleOut: "scaleOut 200ms ease",
