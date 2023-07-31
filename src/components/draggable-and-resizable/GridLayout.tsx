94c3e2fdbd8d03b3c0eaa4f1dd3e26a9f54f4d4c
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2023-07-31 15:42:33 +0330

    React-Grid-Layout re-render columns on parent's resizing

--- src/components/draggable-and-resizable/GridLayout.tsx
+++ src/components/draggable-and-resizable/GridLayout.tsx
@@ -1,8 +1,11 @@
-import { ReactNode } from "react";
+import { useAppSelector } from "@/hooks/useRedux";
+import { ReactNode, useEffect } from "react";
 import { Layout, Layouts, Responsive, WidthProvider } from "react-grid-layout";
 
+const ResponsiveGridLayout = WidthProvider(Responsive);
+
 export default function GridLayout(props: { children: ReactNode; layouts: Layouts }) {
-	const ResponsiveGridLayout = WidthProvider(Responsive);
+	const { isSidebarOpen } = useAppSelector((state) => state.uiConfig);
 
 	const handleLayoutChange = (currentLayout: Layout[], allLayouts: Layouts): void => {
 		console.log("LAYOUT CHANGED!!");
@@ -13,12 +16,23 @@ export default function GridLayout(props: { children: ReactNode; layouts: Layout
 		// localStorage.setItem("grid-layout", JSON.stringify(layouts));
 	};
 
+	useEffect(() => {
+		// when isSidebarOpen changes, we need to dispatch a resize event to the window to force the grid to re-render
+		const timerId = setTimeout(() => {
+			window.dispatchEvent(new Event("resize"));
+		}, 300);
+
+		return () => {
+			clearTimeout(timerId);
+		};
+	}, [isSidebarOpen]);
+
 	return (
 		<div className="p-2 overflow-hidden">
 			<ResponsiveGridLayout
 				layouts={props.layouts}
 				breakpoints={{ lg: 1200, md: 996, sm: 768, xs: 480, xxs: 0 }}
-				cols={{ lg: 4, md: 4, sm: 3, xs: 1, xxs: 1 }}
+				cols={{ lg: 4, md: 4, sm: 3, xs: 2, xxs: 1 }}
 				rowHeight={100}
 				width={1200}
 				onLayoutChange={handleLayoutChange}
